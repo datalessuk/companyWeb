06353f590c93464bd50c9709d28ebb8a91e96151
datalessuk <dataless@gmail.com>
2021-03-02 20:30:07 +0000

    added google maps

--- myScript.js
+++ myScript.js
@@ -1,1 +1,11 @@
+let map;
 
+function initMap() {
+    let hull = {lat: 53.7664164, lng: 0.3670049};
+
+    let map = new google.maps.Map(document.getElementById("map"), {
+        center:hull,
+        zoom: 9,
+    });
+    
+}
