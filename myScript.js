0d1110b5966ea666c078a9160c02180dffa92064
datalessuk <dataless@gmail.com>
2021-03-03 19:51:40 +0000

    added map marker

--- myScript.js
+++ myScript.js
@@ -1,11 +1,16 @@
 let map;
 
 function initMap() {
-    let locarion = {lat: 53.7664164, lng: 0.3670049};
-
+    let location = {lat: 53.7664164, lng: 0.3670049};
+    let marker = {lat:53.7676,lng:-0.3174};
     let map = new google.maps.Map(document.getElementById("map"), {
-        center:locarion,
+        center:location,
         zoom: 9,
+    });
+    new google.maps.Marker({
+        position: marker,
+        map,
+        title:"Here we are",
     });
     
 }
