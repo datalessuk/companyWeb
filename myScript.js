c2cb8624a54650bb65753bc1b21c32bcf5ec156a
datalessuk <dataless@gmail.com>
2021-03-03 20:15:47 +0000

    tweaks to css

--- myScript.js
+++ myScript.js
@@ -1,7 +1,10 @@
 let map;
 
 function initMap() {
-    let location = {lat: 53.7664164, lng: 0.3670049};
+    //location for main hull area 
+    //marker for center of hull
+
+    let location = {lat: 53.7664164, lng: -0.10049};
     let marker = {lat:53.7676,lng:-0.3174};
     let map = new google.maps.Map(document.getElementById("map"), {
         center:location,
